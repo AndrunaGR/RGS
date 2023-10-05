d4a7f362970ed7172b35113c3b7b055a91c52856
AndrunaGR <100125278+AndrunaGR@users.noreply.github.com>
2023-10-05 20:16:17 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let tg = window.Telegram.WebApp;
+tg.expand();
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#FF00FF";
+let btn = document.getElementById("btn");
+btn.addEventListener("click", function(){
+	tg.MainButton.setText("Отправлено");
+	tg.MainButton.show();
+	tg.sendDate("sendTestMessage");
+});
